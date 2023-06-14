0afc788775e49f02f6337e86ec2c857e60b73af6
yeimer lopez <yeimerlopez1216@gmail.com>
2023-06-13 23:00:28 -0500

    commit inicial hasta  la vinculacion de la cadena de coneccion

---
+++ src/test/java/proyectoFinal/MSSeguridad/MsSeguridadApplicationTests.java
@@ -0,0 +1,13 @@
+package proyectoFinal.MSSeguridad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSeguridadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
